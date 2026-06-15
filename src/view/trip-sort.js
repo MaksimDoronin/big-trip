54a613ab3c1fe6c475effd86f440cfe18308a0c6
MaksimDoronin <maksdoroninone@yandex.ru>
2026-06-15 23:01:58 +0300

    feat(frontend): добавить компонент сортировки точек маршрута

---
+++ src/view/trip-sort.js
@@ -0,0 +1,61 @@
+import { createElement } from '../render.js';
+
+function createTripSortTemplate() {
+  return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+      <div class="trip-sort__item  trip-sort__item--day">
+        <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
+        <label class="trip-sort__btn" for="sort-day">Day</label>
+      </div>
+
+      <div class="trip-sort__item  trip-sort__item--event">
+        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" disabled>
+        <label class="trip-sort__btn" for="sort-event">Event</label>
+      </div>
+
+      <div class="trip-sort__item  trip-sort__item--time">
+        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+        <label class="trip-sort__btn" for="sort-time">Time</label>
+      </div>
+
+      <div class="trip-sort__item  trip-sort__item--price">
+        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+        <label class="trip-sort__btn" for="sort-price">Price</label>
+      </div>
+
+      <div class="trip-sort__item  trip-sort__item--offer">
+        <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled>
+        <label class="trip-sort__btn" for="sort-offer">Offers</label>
+      </div>
+    </form>`;
+}
+
+export default class TripSort {
+  getTemplate() {
+    return createTripSortTemplate();
+  }
+
+  getElement() {
+    /*
+     * Паттерн lazy initialization (отложенная инициализация).
+     * 1. Конструктора нет → при new TripSort объект пустой, this.element = undefined.
+     * 2. Первый вызов tripSort.getElement():
+     *    — if (!this.element) → !undefined → true
+     *    — this.element = createElement(this.getTemplate()) — создаём DOM-узел один раз
+     *      и кэшируем в this.element.
+     *    — Возвращаем this.element.
+     *
+     * 3. Второй вызов tripSort.getElement():
+     *    — if (!this.element) → !DOMNode → false
+     *    — Ветка пропускается, сразу возврат this.element (существующий узел).
+     */
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
