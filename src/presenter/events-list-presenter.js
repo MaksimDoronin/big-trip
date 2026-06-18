e389a4c2668caf218033b18f66d1b6ad5d627c3f
MaksimDoronin <maksdoroninone@yandex.ru>
2026-06-18 16:22:24 +0300

    feat(view): добавить карточку события и формы создания/редактирования

--- src/presenter/events-list-presenter.js
+++ src/presenter/events-list-presenter.js
@@ -1,27 +1,20 @@
-// компонент-обёртка (<ul class="trip-events__list">) для будущих точек маршрута.
 import EventsListView from '../view/events-list-view.js';
-// компонент панели сортировки
-// (<form class="trip-events__trip-sort  trip-sort" action="#" method="get">).
 import SortView from '../view/sort-view.js';
 import { render } from '../render';
+import EventsItemView from '../view/events-item-view.js';
+import EventView from '../view/event-view.js';
 
 export default class EventsListPresenter {
-  // Создаёт экземпляр View-компонента EventsListView один раз при инициализации класса.
-  // Сам DOM-элемент <ul class="trip-events__list"> будет создан лениво при первом getElement()
-  // в init().
   eventsListComponent = new EventsListView();
-  // Принимает { eventListContainer } (<section class="trip-events">) — DOM-элемент
-  // , в который будет встроен список.
+  eventsItemComponent = new EventsItemView();
   constructor({ eventsListContainer }) {
     this.eventsListContainer = eventsListContainer;
   }
 
   init() {
-    // встраивает <ul class="trip-events__list"> списка в
-    // указанный контейнер на странице <section class="trip-events">.
+    render(new SortView(), this.eventsListContainer);
     render(this.eventsListComponent, this.eventsListContainer);
-    // создаёт панель сортировки (<form class="trip-events__trip-sort  trip-sort" action="#" method="get">)
-    // и помещает её внутрь контейнера списка <ul class="trip-events__list">
-    render(new SortView(), this.eventsListComponent.getElement());
+    render(this.eventsItemComponent, this.eventsListComponent.getElement());
+    render(new EventView(), this.eventsItemComponent.getElement());
   }
 }
--- src/view/events-list-view.js
+++ src/view/events-list-view.js
@@ -10,19 +10,6 @@ export default class EventsListView {
   }
 
   getElement() {
-    /*
-     * Паттерн lazy initialization (отложенная инициализация).
-     * 1. Конструктора нет → при new EventsListView объект пустой, this.element = undefined.
-     * 2. Первый вызов EventsListView.getElement():
-     *    — if (!this.element) → !undefined → true
-     *    — this.element = createElement(this.getTemplate()) — создаём DOM-узел один раз
-     *      и кэшируем в this.element.
-     *    — Возвращаем this.element.
-     *
-     * 3. Второй вызов EventsListView.getElement():
-     *    — if (!this.element) → !DOMNode → false
-     *    — Ветка пропускается, сразу возврат this.element (существующий узел).
-     */
     if (!this.element) {
       this.element = createElement(this.getTemplate());
     }
