40ba9181641e58f5cff36ff44e993118992b882b
MaksimDoronin <maksdoroninone@yandex.ru>
2026-07-16 19:01:51 +0300

    chore: добавить фото-заглушки в форму создания и убрать пробный комментарий

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import FiltersView from './view/filters-view.js';
 import { render } from './render.js';
 import EventsListPresenter from './presenter/events-list-presenter.js';
-// Пробный комментарий.
+
 const tripControlsFilters = document.querySelector('.trip-controls__filters');
 const sectionElementTripEvents = document.querySelector('.trip-events');
 
 const eventsListPresenter = new EventsListPresenter({
   eventsListContainer: sectionElementTripEvents,
 });
+
 render(new FiltersView(), tripControlsFilters);
 
 eventsListPresenter.init();
