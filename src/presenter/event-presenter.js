56ebaad2aca8cce1cca5d79d65dc44b8b388578b
MaksimDoronin <maksdoroninone@yandex.ru>
2026-07-18 20:52:00 +0300

    refactor(presenter): вынести логику точки в EventPresenter и отрендерить форму создания

--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -1,6 +1,30 @@
-import EventView from '../view/event-view';
+import EventView from '../view/event-view.js';
 
 export default class EventPresenter {
-  eventContainer = new EventView();
-  constructor() {}
+  container = null;
+  model = null;
+  point = null;
+
+  constructor({ container, model, point }) {
+    this.container = container;
+    this.model = model;
+    this.point = point;
+  }
+
+  init() {
+    const destinations = this.model.getDestinations();
+    const offersByType = this.model.getOffersByType();
+
+    const destination = destinations.find((item) => item.id === this.point.destinationId);
+    const offers = (offersByType[this.point.type] || [])
+      .filter((offer) => this.point.offerIds.includes(offer.id));
+
+    const eventView = new EventView({ point: this.point, destination, offers });
+
+    const listItem = document.createElement('li');
+    listItem.classList.add('trip-events__item');
+    listItem.appendChild(eventView.getElement());
+
+    this.container.appendChild(listItem);
+  }
 }
--- src/presenter/events-list-presenter.js
+++ src/presenter/events-list-presenter.js
@@ -1,36 +1,36 @@
 import EventsListView from '../view/events-list-view.js';
 import SortView from '../view/sort-view.js';
-import { render } from '../render';
-import EventsItemView from '../view/events-item-view.js';
-import EventView from '../view/event-view.js';
+import PointEditView from '../view/point-edit-view.js';
+import { render } from '../render.js';
+import EventPresenter from './event-presenter.js';
 
 export default class EventsListPresenter {
   eventsListComponent = new EventsListView();
-  eventsItemComponent = new EventsItemView();
+
   constructor({ eventsListContainer, model }) {
     this.eventsListContainer = eventsListContainer;
     this.model = model;
   }
 
   init() {
-    this.listPoints = [...this.model.getPoints()];
-    const destinations = this.model.getDestinations();
-    const offersByType = this.model.getOffersByType();
-
     render(new SortView(), this.eventsListContainer);
     render(this.eventsListComponent, this.eventsListContainer);
-    render(this.eventsItemComponent, this.eventsListComponent.getElement());
 
-    for (let i = 0; i < this.listPoints.length; i++) {
-      const point = this.listPoints[i];
-      const destination = destinations.find((item) => item.id === point.destinationId);
-      const offers = (offersByType[point.type] || [])
-        .filter((offer) => point.offerIds.includes(offer.id));
+    const editForm = new PointEditView({
+      offersByType: this.model.getOffersByType(),
+      types: this.model.getTypes(),
+    });
+    const formListItem = document.createElement('li');
+    formListItem.classList.add('trip-events__item');
+    formListItem.appendChild(editForm.getElement());
+    this.eventsListComponent.getElement().appendChild(formListItem);
 
-      render(
-        new EventView({ point, destination, offers }),
-        this.eventsItemComponent.getElement(),
-      );
+    for (const point of this.model.getPoints()) {
+      new EventPresenter({
+        container: this.eventsListComponent.getElement(),
+        model: this.model,
+        point,
+      }).init();
     }
   }
 }
